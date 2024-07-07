1c9145bf711f580103c6e836ac2d4c73d77091de
QuangBuiCP <buithanhquang2006@gmail.com>
2024-07-07 16:28:07 +0700

    chore + upd: reorganize with OOP implementation

---
+++ src/render_app.h
@@ -0,0 +1,28 @@
+#ifndef RENDER_H_
+#define RENDER_H_
+
+#include "olcPixelGameEngine.h"
+
+class pgeDescartes : public olc::PixelGameEngine {
+ public:
+  pgeDescartes();
+
+ private:
+  olc::vf2d offset;
+  olc::vf2d start_pan;
+  float scale;
+  float grid_edge;
+
+  olc::vf2d cursor;
+  olc::vf2d last_cursor;
+
+  void WorldToScreen(const olc::vf2d& v, int& nScreenX, int& nScreenY);
+  void ScreenToWorld(int nScreenX, int nScreenY, olc::vf2d& v);
+  void MyDrawString(int x, int y, std::string s, olc::Pixel color, int nScale);
+
+ public:
+  bool OnUserCreate() override;
+  bool OnUserUpdate(float fElapsedTime) override;
+};
+
+#endif // RENDER_H_
